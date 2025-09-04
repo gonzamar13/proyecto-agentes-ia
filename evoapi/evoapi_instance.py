6b317ec627db76d52845e6a0df966c79effc0d7e
marcos <marcosgonzs13@gmail.com>
2025-09-04 15:04:50 -0300

    Se crea un metodo para controlar el try/except

--- evoapi/evoapi_instance.py
+++ evoapi/evoapi_instance.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import requests
 import os
+from requests.exceptions import HTTPError, ConnectionError, RequestException, Timeout
 
 class EvoAPI:
     def __init__(self, url_evoapi, api_key):
@@ -9,10 +10,23 @@ class EvoAPI:
                         "apikey":api_key, 
                         "Content-Type": "application/json"
                         }
-        
-    def create_instance(self, name_instance, token, qrcode, number, integration):
-        url = f"{self.url_evoapi}/instance/create"  
 
+    def _request(self, method:str , endpoint:str, **kwargs) -> dict:
+            url = f"{self.url_evoapi}{endpoint}"  
+            try: 
+                response = requests.request(method, url, headers=self.headers, timeout=10, **kwargs)
+                response.raise_for_status()
+                return response.json()
+            except HTTPError as http_error:
+                return {"Error": f"HTTP error: {http_error}", "status": response.status_code}
+            except ConnectionError:
+                return {"Error": "No se pudo establecer contacto con el servidor de evoapi"}
+            except Timeout:
+                return {"Error": "La solicitud al servidor demoro demasiado y expiro"}
+            except RequestException as req_err:
+                return {"Error": f"Ocurrio un error inesperado: {req_err}"}
+
+    def create_instance(self, name_instance, token, qrcode, number, integration):
         payload = {
                    "instanceName":name_instance, 
                    "token":token, 
@@ -20,15 +34,13 @@ class EvoAPI:
                    "number":number,
                    "integration": integration,
                     }
-        respose = requests.post(url, json=payload, headers=self.headers)
-
-        return respose.json()
+        return self._request("POST", "/instance/create", json=payload)
 
 load_dotenv()
 
 URL_EVOAPI = os.getenv("BASE_URL")
 KEY_EVOAPI = os.getenv("API_KEY_EVOLU")
-NAME_INTANCE = "Nueva_intancia_prueba_1"
+NAME_INSTANCE = "Nueva_intancia_prueba_2"
 NUMBER_PHONE = "595991805304"
 TOKEN = "JAKLJSD423490293J"
 INTEGRATION = "WHATSAPP-BAILEYS"
@@ -37,6 +49,6 @@ QR = True
 print(URL_EVOAPI)
 
 api_client = EvoAPI(URL_EVOAPI, KEY_EVOAPI)
-instance_create = api_client.create_instance(NAME_INTANCE, TOKEN, QR, NUMBER_PHONE, INTEGRATION)
+instance_create = api_client.create_instance(NAME_INSTANCE, TOKEN, QR, NUMBER_PHONE, INTEGRATION)
 print(instance_create)
     
